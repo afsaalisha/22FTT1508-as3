c32b97e2c1f205a7da1af6fe0f23c5b49481f00e
afsaalisha <22ftt1508@student.pb.edu.bn>
2024-04-18 13:41:13 +0800

    changed font to bold

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <h1 class="text-3xl md:text-5xl">AFSA ALISHA</h1>
     <br>
     <img src="./assets/shwek.jpg" class="profile-pic w-40 h-40 md:w-64 md:h-64 rounded-full mb-8" alt="Profile Pic" />
-    <p class="text-lg md:text-xl">I love Shrek more than you.</p>
+    <p class="text-lg md:text-xl font-bold">I love Shrek more than you.</p>
     <p class="text-lg md:text-xl">I am a student at Politeknik Brunei, studying Information Technology.</p>
     <p class="text-lg md:text-xl">My hobbies include playing games, watching movies, and listening to music.</p>
     <p class="text-lg md:text-xl">My favorite movie is <a href="https://www.dreamworks.com/movies/shrek" target="_blank" rel="noopener" class="text-blue-500 hover:text-blue-700">Shrek</a>.</p>
